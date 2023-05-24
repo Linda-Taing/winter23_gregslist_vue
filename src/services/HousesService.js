2eeb29bc0bb01532c220944cc281fb2a37841c36
Linda Taing <lindatdoan@me.com>
2023-05-23 21:06:53 -0600

    removed auth from url

--- src/services/HousesService.js
+++ src/services/HousesService.js
@@ -5,18 +5,18 @@ import { logger } from "../utils/Logger.js";
 
 class HousesService {
     async getHouses() {
-        const res = await api.get('auth/api/houses')
+        const res = await api.get('api/houses')
         // logger.log('[get houses]', res.data)
         AppState.houses = res.data
         // logger.log(AppState.houses);
     }
     async createHouse(houseData) {
-        const res = await api.post('auth/api/houses', houseData)
+        const res = await api.post('api/houses', houseData)
         AppState.houses.push(res.data)
         return res.data
     }
     async editHouse(houseData) {
-        const res = await api.put('auth/api/houses/' + houseData.id, houseData)
+        const res = await api.put('api/houses/' + houseData.id, houseData)
         let i = AppState.houses.findIndex(c => c.id == houseData.id)
         if (i == -1) {
             throw new Error('Sorry Nobody is Home...')
@@ -24,7 +24,7 @@ class HousesService {
         AppState.houses.splice(i, 1, res.data)
     }
     async removeHouse(houseId) {
-        const res = await api.delete('auth/api/houses/' + houseId)
+        const res = await api.delete('api/houses/' + houseId)
         let i = AppState.houses.findIndex(h => h.id == houseId)
         if (i != -1) {
             AppState.houses.splice(i, 1)
@@ -32,7 +32,7 @@ class HousesService {
     }
     async getHouseById(houseId) {
         AppState.house = null
-        const res = await api.get('auth/api/houses/' + houseId)
+        const res = await api.get('api/houses/' + houseId)
         AppState.house = res.data
     }
 
