aa5a973c0c554799d83ad3f968a47dc4eee88337
Linda Taing <lindatdoan@me.com>
2023-05-23 21:51:56 -0600

    vue warning is keeping anything from loading onto the DOM

--- src/components/CarCard.vue
+++ src/components/CarCard.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="CarCard card">
     <router-link :to="{ name: 'Car', params: { carId: car.id } }">
-      <img class="rounded-top img-fluid w-100" :src="car.imgUrl" :alt="car.make + ' ' + car.model">
+      <img class="rounded-top img-fluid w-100" :src="car?.imgUrl" :alt="car?.make + ' ' + car?.model">
     </router-link>
 
     <div class="card-body">
-      <p><b>{{ car.make }} {{ car.model }} - ${{ car.price }}</b></p>
+      <p><b>{{ car?.make }} {{ car?.model }} - ${{ car?.price }}</b></p>
     </div>
     <div class="card-footer text-end" v-if="showSeller">
       <span class="on-hover me-2">{{ car?.seller.name }}</span>
-      <img :src="car.seller.picture" :alt="car.seller.name" :title="car.seller.name" height="40" width="40"
+      <img :src="car?.seller.picture" :alt="car?.seller.name" :title="car?.seller.name" height="40" width="40"
         class="rounded-circle">
     </div>
   </div>
--- src/services/CarsService.js
+++ src/services/CarsService.js
@@ -11,14 +11,14 @@ class CarsService {
   }
 
   async createCar(carData) {
-    const res = await api.post('auth/api/cars', carData)
+    const res = await api.post('api/cars', carData)
     AppState.cars.push(res.data)
     return res.data
     // TODO something fancy with the router
   }
 
   async editCar(carData) {
-    const res = await api.put('auth/api/cars/' + carData.id, carData)
+    const res = await api.put('api/cars/' + carData.id, carData)
     let i = AppState.cars.findIndex(c => c.id == carData.id)
     if (i == -1) {
       throw new Error('umm wat!?!')
@@ -28,12 +28,12 @@ class CarsService {
 
   async getCarById(carId) {
     AppState.car = null
-    const res = await api.get('auth/api/cars/' + carId)
+    const res = await api.get('api/cars/' + carId)
     AppState.car = res.data
   }
 
   async removeCar(carId) {
-    const res = await api.delete('auth/api/cars/' + carId)
+    const res = await api.delete('api/cars/' + carId)
     let i = AppState.cars.findIndex(c => c.id == carId)
     if (i != -1) {
       AppState.cars.splice(i, 1)
